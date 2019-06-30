18f9825d00b149178938e5e65ae285b088897f63
duckthecuck <38166539+duckthecuck@users.noreply.github.com>
2019-06-30 13:05:59 -0700

    Update Commands.js

--- Commands.js
+++ Commands.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
-const help = ('**!duckinfo** - Displays info about the bot, code, and Acknowledgements \n **!gitlist <unit>** - Displays the full stats of any units with the matching name \n **!git <unit>** - Displays a message where you can scroll through the units using reactions \n **!gitpm <unit>** - Same as git but sends the results through pm \n **!gitspec <unit>** - Same as git but requires a specific unit name \n **!list <unit>** - Lists all matching units \n **!ke <ke value>** - Displays a table of armor damage values for that ke value \n **!heat <heat value>** - Displays a table of armor damage for that heat value \n **!armor <0 - 25 armor>** - Displays the damage resistance of an armor value towards ke and heat \n **!<map>** - Displays a map of a ranked map \n **!wiki / !wikipedia <search term>** - searches and returns a description of a wikipedia article - ***please make sure you wait for the reactions to complete before clicking on one, or it wont work*** \n  **!vet** - Shows Vlern\'s table of accuracy with upvetting \n **!userinvite** - Makes a 2 hour, 1 use invite for you to invite someone \n **!rookie** - Gives you the rookie role \n **!lfg** - Adds you to the looking for game pool \n **!unspecguide** - A beginner deck building guide \n **!specprimer** - A primer to spec decks and how to counter them \n **!honguide** - A beginner\'s guide to wargame \n **!razzguide** - Razzmann\'s video wargame guides \n **!keyvalues** - Values worth remembering  \n **!armorytool** - A tool for viewing hidden unit stats \n **!replayfolder** - Folder Where game replays are stored \n **!rof** - A Rate of Fire cheatsheet \n **!bling** - How to get colors and tags in wargame \n **!progression** - Reccomended progression guide for beginners\n **!rankedmaps** - List of maps in the ranked pool \n\n **Changelog**: https://docs.google.com/document/d/1WAlUqnyoh8ZZCVZwXd95em5sXpuMG4iSiE2ODPG7iSE/edit?usp=sharing \n\n **For any bugs or questions, pm senorDickweed#7033**');
+const help = ('**!duckinfo** - Displays info about the bot, code, and Acknowledgements \n **!gitlist <unit>** - Displays the full stats of any units with the matching name \n **!git <unit>** - Displays a message where you can scroll through the units using reactions \n **!gitpm <unit>** - Same as git but sends the results through pm \n **!gitspec <unit>** - Same as git but requires a specific unit name \n **!list <unit>** - Lists all matching units \n **!ke <ke value>** - Displays a table of armor damage values for that ke value \n **!heat <heat value>** - Displays a table of armor damage for that heat value \n **!armor <0 - 25 armor>** - Displays the damage resistance of an armor value towards ke and heat \n **!<map>** - Displays a map of a ranked map \n  **!vet** - Shows Vlern\'s table of accuracy with upvetting \n **!userinvite** - Makes a 2 hour, 1 use invite for you to invite someone \n **!rookie** - Gives you the rookie role \n **!lfg** - Adds you to the looking for game pool \n **!unspecguide** - A beginner deck building guide \n **!specprimer** - A primer to spec decks and how to counter them \n **!honguide** - A beginner\'s guide to wargame \n **!razzguide** - Razzmann\'s video wargame guides \n **!keyvalues** - Values worth remembering  \n **!armorytool** - A tool for viewing hidden unit stats \n **!replayfolder** - Folder Where game replays are stored \n **!rof** - A Rate of Fire cheatsheet \n **!bling** - How to get colors and tags in wargame \n **!progression** - Reccomended progression guide for beginners\n **!rankedmaps** - List of maps in the ranked pool \n\n **Changelog**: https://docs.google.com/document/d/1WAlUqnyoh8ZZCVZwXd95em5sXpuMG4iSiE2ODPG7iSE/edit?usp=sharing \n\n **For any bugs or questions, pm senorDickweed#7033**');
 const adminhelp = ('List of admin commands: \n**!invite <duration in minutes> <uses>** - Creates an invite link, set duration to zero to make it infinite duration \n **!changelimit <number>** - Changes the limit of matching units to display fully \n **!changedisplaylimit <number>** - Changes the limit of units to be shown in a name list \n **!dynocommands** - Turns on / off the dyno commands (!unspec !rookie, etc)');
-const MarkovGen = require('markov-generator');
 const mongoose = require('mongoose');
 const MSGES = require('./Mongoose/messageSchema.js');
 
@@ -188,17 +187,4 @@ module.exports.botcommands = (client, admin) => {
         });
   };
 
-  module.exports.imitate = (args, message, allmessages) => {
-    console.log(message);
-        let markovstrings = [];
-        for (i of allmessages) {
-            markovstrings.push(i.message_content)
-        }
-        let markov = new MarkovGen({
-          input: markovstrings,
-          minLength: 30
-        });
-         
-        let sentence = markov.makeChain();
-                message.channel.send(sentence);
-  }
+
